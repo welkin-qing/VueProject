17705d01efc8dbdb2b5be91b51f90181e59a26a7
welkin-qing <welkin.qing@foxmail.com>
2018-08-27 13:38:45 +0800

    项目首页基本样式

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
 // 入口文件
-console.log('OK')
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
 import app from './App.vue'
 
 //按需导入mint-ui的组件header
-import { Header } from 'mint-ui';
+import { Header ,Swipe, SwipeItem} from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 //导入mui
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+//导入router.js路由模块
+import router from './router.js'
+//导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 })
